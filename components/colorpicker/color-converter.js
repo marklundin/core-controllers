a695206853541a476836bda2e0f4267decb3968e
Mark Lundin <mark@mark-lundin.com>
2016-04-29 18:46:12 +0100

    Added converter for array based rgb colors

--- components/colorpicker/color-converter.js
+++ components/colorpicker/color-converter.js
@@ -2,10 +2,12 @@ import { rgb, hsl } from './prop-types'
 import Colr from 'colr'
 
 export let rgb2Hsv = c => Colr.fromRgbObject(c).toRawHsvObject()
+export let rgbArr2Hsv = c => Colr.fromRgbArray(c).toRawHsvObject()
 export let hsl2Hsv = c => Colr.fromHslObject(c).toRawHsvObject()
 export let hsv2Hsv = c => c
 
 rgb2Hsv.invert = c => Colr.fromHsvObject(c).toRawRgbObject()
+rgbArr2Hsv.invert = c => Colr.fromHsvObject(c).toRawRgbArray()
 hsl2Hsv.invert = c => Colr.fromHsvObject(c).toRawHslObject()
 hsv2Hsv.invert = c => c
 
